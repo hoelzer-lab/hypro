8b263cbf3b25c925b03f05d39f0e295b9b8aab13
Maximilian Arlt <ha74mit@sanon.bioinf.uni-jena.de>
2019-11-06 18:27:36 +0100

    wrapper for all mmseqs2 cmds, required by gff_extend.py

--- scripts/mmseqs2.sh
+++ scripts/mmseqs2.sh
@@ -2,6 +2,7 @@
 # 22.10.19
 # script serves to find hypothetical protein (hyprot) annotation from prokka
 # input: query fasta with hyprots
+
 path="$1"
 QFASTA="${path}/../query.fasta"
 TFASTA="$2"						#"${path}/db/uniprotkb/uniprot_sprot.fasta"
@@ -10,13 +11,6 @@ TARGETDB="$3"						#"${path}/db/uniprotkb/target_db"
 RESULTDB="${path}/results_db"
 TMP="${path}/tmp"
 OUT="${path}/mmseq2_out.tsv"
-
-# echo ${QUERYDB}
-# echo ${TARGETDB}
-# echo ${RESULTDB}
-# echo ${TMP}
-# echo ${OUT}
-# head ${QFASTA}
 
 mmseqs createdb ${QFASTA} ${QUERYDB}
 mmseqs createdb ${TFASTA} ${TARGETDB}
