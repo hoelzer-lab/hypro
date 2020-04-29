a03c40ae123a29f76b12f9f1c99e5eda5d456484
Maximilian Arlt <ha74mit@dessert.bioinf.uni-jena.de>
2020-04-29 18:05:35 +0200

    #24, #18, #6

--- scripts/mmseqs2.sh
+++ scripts/mmseqs2.sh
@@ -4,8 +4,8 @@
 # input: query fasta with hyprots
 
 function is_indexed(){
-	if [ ${DBTYPE} == 'uniprotkb' ] # if clause to decide for the type of db that was chosen- default is uniprotkb (see gff_extend.py)
-	then
+	#if [ ${DBTYPE} == 'uniprotkb' ] # if clause to decide for the type of db that was chosen- default is uniprotkb (see gff_extend.py)
+	#then
 		index_files=('target_db.idx' 'target_db.idx.dbtype' 'target_db.idx.index')
 		DN=$(dirname ${2})
 		for file in ${index_files[@]}
@@ -26,15 +26,15 @@ function is_indexed(){
 		done
 		# echo ${DN}
 		return 0
-	else
-		echo 'Unknown database type.'
-	fi
+	# else
+	# 	echo 'Unknown database type.'
+	# fi
 
 }
 
 function created_querydb(){
-	if [ ${DBTYPE} == 'uniprotkb' ] # if clause to decide for the type of db that was chosen- default is uniprotkb (see hypro.py)
-	then
+	# if [ ${DBTYPE} == 'uniprotkb' ] # if clause to decide for the type of db that was chosen- default is uniprotkb (see hypro.py)
+	# then
 		index_files=('query_db' 'query_db.dbtype' 'query_db.index' 'query_db.lookup' 'query_db_h' 'query_db_h.dbtype' 'query_db_h.index')
 		DN=$(dirname ${2})
 		for file in ${index_files[@]}
@@ -54,15 +54,15 @@ function created_querydb(){
 		done
 		# echo ${DN}
 		return 0
-	else
-		echo 'Unknown database type.'
-	fi
+	# else
+	# 	echo 'Unknown database type.'
+	# fi
 
 }
 
 function created_resultsdb(){
-	if [ ${DBTYPE} == 'uniprotkb' ] # if clause to decide for the type of db that was chosen- default is uniprotkb (see gff_extend.py)
-	then
+	# if [ ${DBTYPE} == 'uniprotkb' ] # if clause to decide for the type of db that was chosen- default is uniprotkb (see gff_extend.py)
+	# then
 		index_files=('target_db.index' 'target_db.lookup' 'target_db_h' 'target_db_h.dbtype' 'target_db_h.index')
 		DN=$(dirname ${2})
 		for file in ${index_files[@]}
@@ -82,9 +82,9 @@ function created_resultsdb(){
 		done
 		echo ${DN}
 		return 0
-	else
-		echo 'Unknown database type.'
-	fi
+	# else
+	# 	echo 'Unknown database type.'
+	# fi
 
 }
 
@@ -99,8 +99,10 @@ DBTYPE="$4"
 EVAL="$5"						# minimal E-value - parameter "-e"
 ALEN="$6"						# minimal alignment length "--min-aln-len"
 PIDENT="$7"						# minimal percent identity - "--min-seq-id"
-RESULTDB="${RPREFIX}/e${EVAL}_a${ALEN}_p${PIDENT}" # generate an individual resultsdb for every parameter setting, since the results db is not overwritten by mmseqs search
-OUT="${path}/final_outs/mmseqs2_out_e${EVAL}_a${ALEN}_p${PIDENT}.tsv"
+THREADS="$8"					# number of threads to use
+# SENS="$9"						# adjust sensitivity of mmseqs
+RESULTDB="${RPREFIX}/db_${DBTYPE}_e${EVAL}_a${ALEN}_p${PIDENT}" # generate an individual resultsdb for every parameter setting, since the results db is not overwritten by mmseqs search
+OUT="${path}/final_outs/mmseqs2_out_db_${DBTYPE}_e${EVAL}_a${ALEN}_p${PIDENT}.tsv"
 
 rm -r ${RPREFIX}
 
@@ -127,22 +129,20 @@ then
 	mmseqs createdb ${TFASTA} ${TARGETDB}
 fi
 
-
-
 is_indexed ${DBTYPE} ${TARGETDB}      # if index files of taget_db exist and non-zero, skip
 if [ "$?" -eq 0 ]
 then
 	echo 'Found a valid index. Indexing skipped...'
 elif [ "$?" -eq 1 ]
 then
-	mmseqs createindex ${TARGETDB} ${TMP}
+	mmseqs createindex ${TARGETDB} ${TMP} --threads ${THREADS}
 fi
 
-mmseqs search ${QUERYDB} ${TARGETDB} ${RESULTDB} ${TMP} -e ${EVAL} --min-aln-len ${ALEN} --min-seq-id ${PIDENT}
-mmseqs convertalis --format-mode 0 --format-output 'query,target,pident,alnlen,mismatch,gapopen,qlen,qstart,qend,tstart,tend,evalue,bits' ${QUERYDB} ${TARGETDB} ${RESULTDB} ${OUT}
+mmseqs search ${QUERYDB} ${TARGETDB} ${RESULTDB} ${TMP} --threads ${THREADS} -e ${EVAL} --min-aln-len ${ALEN} --min-seq-id ${PIDENT}
+mmseqs convertalis --threads ${THREADS} --format-mode 0 --format-output 'query,target,pident,alnlen,mismatch,gapopen,qlen,qstart,qend,tstart,tend,evalue,bits' ${QUERYDB} ${TARGETDB} ${RESULTDB} ${OUT}
 
 head ${OUT}
 echo "Generate unique table with highest bit scores from raw mmseq2 output..."
-echo "query	target	pident	alnlen	mismatch	gapopen	qlen	qstart	qend	tstart	tend	evalue	bits" > "${path}/final_outs/mmseqs2_out_e${EVAL}_a${ALEN}_p${PIDENT}_unique.tsv" # add header
-sort -u -k1,1 ${OUT} >> "${path}/final_outs/mmseqs2_out_e${EVAL}_a${ALEN}_p${PIDENT}_unique.tsv"
-head "${path}/final_outs/mmseqs2_out_e${EVAL}_a${ALEN}_p${PIDENT}_unique.tsv"
+echo "query	target	pident	alnlen	mismatch	gapopen	qlen	qstart	qend	tstart	tend	evalue	bits" > "${path}/final_outs/mmseqs2_out_db_${DBTYPE}_e${EVAL}_a${ALEN}_p${PIDENT}_unique.tsv" # add header
+sort -u -k1,1 ${OUT} >> "${path}/final_outs/mmseqs2_out_db_${DBTYPE}_e${EVAL}_a${ALEN}_p${PIDENT}_unique.tsv"
+head "${path}/final_outs/mmseqs2_out_db_${DBTYPE}_e${EVAL}_a${ALEN}_p${PIDENT}_unique.tsv"
